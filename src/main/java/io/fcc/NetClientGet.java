b72bbe294f32736de7270109ef8f3a2080ea5424
Jayprakash Ray <jayprakashray@Jayprakashs-MacBook-Pro.local>
2022-05-30 15:36:28 -0700

    initial commit

---
+++ src/main/java/io/fcc/NetClientGet.java
@@ -0,0 +1,51 @@
+package io.fcc;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class NetClientGet {
+
+
+    public static void main(String[] args) {
+
+        try {
+        	String  accessToken = new LoginNew().doit();
+            URL url = new URL("https://financialdataexchange-prod.apigee.net/fdx-v4_6/accounts?accountIds=Loan252&resultType=details");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+            conn.setRequestProperty("Authorization","Bearer "+accessToken);
+
+            if (conn.getResponseCode() != 200) {
+                throw new RuntimeException("Failed : HTTP error code : "
+                        + conn.getResponseCode());
+            }
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(
+                    (conn.getInputStream())));
+
+            String output;
+            System.out.println("Output from Server .... \n");
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+
+            conn.disconnect();
+
+        } catch (MalformedURLException e) {
+
+            e.printStackTrace();
+
+        } catch (IOException e) {
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
